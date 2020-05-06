b08021917f5963ddd98623f51f0036db2eb196b5
David Boles <me@davidbol.es>
2020-05-06 16:50:30 -0700

    Working converter

--- conversion/convert.js
+++ conversion/convert.js
@@ -1,51 +1,40 @@
 const util = require('util')
 const fs = require('fs')
-const DxfParser = require('dxf-parser')
+const Helper = require('dxf').Helper
 
-const scale = 6
-const plotterOffset = [-5000, 5000]
+const maxPlotMin = [-2500, -2500]
+const maxPlotMax = [2500, 2500]
+const maxPlotSize = [maxPlotMax[0] - maxPlotMin[0], maxPlotMax[1] - maxPlotMin[1]]
 
 const text = fs.readFileSync(process.argv[2], "utf8")
-const parsed = new DxfParser().parseSync(text);
+const helper = new Helper(text)
 
+const out = helper.toPolylines();
+const dxfMin = [out.bbox.min.x, out.bbox.min.y]
+const dxfMax = [out.bbox.max.x, out.bbox.max.y]
+const dxfSize = [dxfMax[0] - dxfMin[0], dxfMax[1] - dxfMin[1]]
 
+const reducePlotWidth = (maxPlotSize[0]/maxPlotSize[1]) > (dxfSize[0]/dxfSize[1])
+const ratioFactor = reducePlotWidth? dxfSize[0]/dxfSize[1]: dxfSize[1]/dxfSize[0];
 
- // [[0/1, 0.01cm int, 0.01cm int], ...]
-const moves = []
+const plotMin = [reducePlotWidth? ratioFactor * maxPlotMin[1] : maxPlotMin[0], reducePlotWidth? maxPlotMin[1] : ratioFactor * maxPlotMin[0]]
+const plotMax = [reducePlotWidth? ratioFactor * maxPlotMax[1] : maxPlotMax[0], reducePlotWidth? maxPlotMax[1] : ratioFactor * maxPlotMax[0]]
+const plotSize = [plotMax[0] - plotMin[0], plotMax[1] - plotMin[1]]
 
-// {x: dxf float, y: dxf float} -> new move
-const addMove = (penDown, pathSegment) => {
-  moves[moves.length] = [penDown? 1 : 0, Math.round((pathSegment.x * scale) + plotterOffset[0]), Math.round((-pathSegment.y * scale) + plotterOffset[1])]
+const scaleFactor = [plotSize[0] / dxfSize[0], plotSize[1] / dxfSize[1]]
+
+const toPlotCoords = (dxfCoords) => {
+  return [Math.round((dxfCoords[0] - dxfMin[0]) * scaleFactor[0] + plotMin[0]), Math.round((-(dxfCoords[1] - dxfMin[1]) + dxfSize[1]) * scaleFactor[1] + plotMin[1])]
 }
 
-// [{x: dxf float, y: dxf float}, ...] -> new move
-const addPath = (path) => {
+const moves = []
+for(let polyline of out.polylines) {
   let first = true
-  for(let segment of path) {
-    addMove(!first, segment)
+  for(let vertex of polyline.vertices) {
+    moves[moves.length] = [first? 0 : 1, ...toPlotCoords(vertex)]
     first = false
   }
 }
-
-const isVisible = (ent) => {
-  return (ent.lineType ? ent.lineType.indexOf('HIDDEN') == -1 : true) && (ent.layer? ent.layer == 'CO3' /*|| ent.layer == 'CO2'*/ : false);
-}
-
-const handlers = {
-  LINE: (ent) => addPath(ent.vertices),
-  LWPOLYLINE: (ent) => addPath(ent.vertices),
-}
-
-for(let entity of parsed.entities) {
-  const handler = handlers[entity.type]
-  if(handler && isVisible(entity)) {
-    console.error("handling ", entity)
-    handler(entity)
-  }else {
-    // console.error("No handler for entity: ", entity)
-  }
-}
-
 moves[moves.length] = [0, 0, 0]
 
 console.log('static short state[][3] = \n{');
@@ -53,6 +42,3 @@ for(let move of moves) {
   console.log(`${move[0]}, ${move[1]}, ${move[2]},`)
 }
 console.log('};')
-
-// console.log(util.inspect(parsed, {showHidden: false, depth: null}))
-//console.log(parsed)
