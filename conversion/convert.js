607ac57e4df51b77eca285ee35d3dafd908167f1
David Boles <me@davidbol.es>
2020-05-06 17:28:40 -0700

    Added conversion instructions

--- conversion/convert.js
+++ conversion/convert.js
@@ -2,18 +2,47 @@ const util = require('util')
 const fs = require('fs')
 const Helper = require('dxf').Helper
 
+
+
+// --- USAGE ---
+/*
+Run with Node.js after running `npm i` to download dependencies.
+The first argument should be a path to a DXF file and the
+corresponding plot header file will be written to stdout.
+For example, from the repository root:
+
+```
+node conversion/convert.js elev.dxf > maps/elev.hh
+```
+
+*/
+
+
+
+// --- CONFIG ---
+
+// The maximum bounds of the area to plot in
 const maxPlotMin = [-3000, -3000]
 const maxPlotMax = [3000, 1500]
-const maxPlotSize = [maxPlotMax[0] - maxPlotMin[0], maxPlotMax[1] - maxPlotMin[1]]
-const plotCenter = [maxPlotMin[0] + (maxPlotSize[0]/2), maxPlotMin[1] + (maxPlotSize[1]/2)]
 
+
+
+// --- CONVERT ---
+
+// Read and parse DXF
 const text = fs.readFileSync(process.argv[2], "utf8")
 const helper = new Helper(text)
 
+// Convert to lines
 const out = helper.toPolylines();
+
+// Determine mapping from DXF coordinates into a nicely scaled plot
 const dxfMin = [out.bbox.min.x, out.bbox.min.y]
 const dxfMax = [out.bbox.max.x, out.bbox.max.y]
 const dxfSize = [dxfMax[0] - dxfMin[0], dxfMax[1] - dxfMin[1]]
+
+const maxPlotSize = [maxPlotMax[0] - maxPlotMin[0], maxPlotMax[1] - maxPlotMin[1]]
+const plotCenter = [maxPlotMin[0] + (maxPlotSize[0]/2), maxPlotMin[1] + (maxPlotSize[1]/2)]
 
 const reducePlotWidth = (maxPlotSize[0]/maxPlotSize[1]) > (dxfSize[0]/dxfSize[1])
 const ratioFactor = reducePlotWidth? dxfSize[0]/dxfSize[1]: dxfSize[1]/dxfSize[0];
@@ -27,6 +56,7 @@ const toPlotCoords = (dxfCoords) => {
   return [Math.round((dxfCoords[0] - dxfMin[0]) * scaleFactor[0] + plotMin[0]), Math.round((-(dxfCoords[1] - dxfMin[1]) + dxfSize[1]) * scaleFactor[1] + plotMin[1])]
 }
 
+// Convert all the polylines into a set of moves
 const moves = []
 for(let polyline of out.polylines) {
   let first = true
@@ -35,8 +65,9 @@ for(let polyline of out.polylines) {
     first = false
   }
 }
-moves[moves.length] = [0, 0, 0]
+moves[moves.length] = [0, 0, 0] // Termination marker
 
+// Output all the moves to stdout
 console.log('static short state[][3] = \n{');
 for(let move of moves) {
   console.log(`${move[0]}, ${move[1]}, ${move[2]},`)
--- part2/plotsim.cc
+++ part2/plotsim.cc
@@ -10,7 +10,11 @@
 #include "timer.hh"
 #include "mysimulator.hh"
 
-#include "elev.hh"
+// #include "ri.hh"
+#include "ak.hh"
+// #include "mi.hh"
+// #include "house_side.hh"
+// #include "elev.hh"
 
 // Qs
 // Not using prescaler
@@ -28,7 +32,7 @@
 #define MIN_PULSES_PER_SECOND 1000.0
 #define MAX_PULSES_PER_SECOND 10000.0
 #define ACCELERATION_TIME_SECS 0.150
-#define SAFETY_FACTOR 0.04
+#define SAFETY_FACTOR 0.03
 
 double_t PULSES_PER_CM = (PULSES_PER_REV / CM_PER_REV);
 double_t MIN_PULSES_PER_TICK = (1 - SAFETY_FACTOR) * (MIN_PULSES_PER_SECOND / CLOCK_FREQ_HZ);
